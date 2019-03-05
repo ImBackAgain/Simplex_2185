b552c090d54209b251ebd4eb8a02bad36ff3a5da
Enan Munzar (RIT Student) <esm6840@ad.rit.edu>
2019-03-05 15:14:11 -0500

    addded the rigidbody contest thing

--- E09 - ARBB/MyRigidBody.cpp
+++ E09 - ARBB/MyRigidBody.cpp
@@ -197,23 +197,23 @@ bool MyRigidBody::IsColliding(MyRigidBody* const other)
 {
 	//check if spheres are colliding
 	bool bColliding = true;
-	//bColliding = (glm::distance(GetCenterGlobal(), other->GetCenterGlobal()) < m_fRadius + other->m_fRadius);
+	bColliding = (glm::distance(GetCenterGlobal(), other->GetCenterGlobal()) < m_fRadius + other->m_fRadius);
 	//if they are check the Axis Aligned Bounding Box
 	if (bColliding) //they are colliding with bounding sphere
 	{
 		if (this->m_v3MaxG.x < other->m_v3MinG.x) //this to the right of other
 			bColliding = false;
-		if (this->m_v3MinG.x > other->m_v3MaxG.x) //this to the left of other
+		else if (this->m_v3MinG.x > other->m_v3MaxG.x) //this to the left of other
 			bColliding = false;
 
-		if (this->m_v3MaxG.y < other->m_v3MinG.y) //this below of other
+		else if (this->m_v3MaxG.y < other->m_v3MinG.y) //this below of other
 			bColliding = false;
-		if (this->m_v3MinG.y > other->m_v3MaxG.y) //this above of other
+		else if (this->m_v3MinG.y > other->m_v3MaxG.y) //this above of other
 			bColliding = false;
 
-		if (this->m_v3MaxG.z < other->m_v3MinG.z) //this behind of other
+		else if (this->m_v3MaxG.z < other->m_v3MinG.z) //this behind of other
 			bColliding = false;
-		if (this->m_v3MinG.z > other->m_v3MaxG.z) //this in front of other
+		else if (this->m_v3MinG.z > other->m_v3MaxG.z) //this in front of other
 			bColliding = false;
 
 		if (bColliding) //they are colliding with bounding box also
